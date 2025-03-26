fbfec6d76122017038defec8ed1daaab49ff3147
Tushar Pathak <97155038+FieryGhoul@users.noreply.github.com>
2025-03-26 11:20:59 +0530

    Update UpDownCounterSW2.c

--- ES/Lab 6/UpDownCounterSW2.c
+++ ES/Lab 6/UpDownCounterSW2.c
@@ -14,21 +14,21 @@ int main(void){
   LPC_GPIO2->FIODIR &= ~(1<<12);    //gpio input
 
   while(1){
-    if((LPC_GPIO1->FIOPIN & (1<<12)) == 0){        //checking if sw2 is clicked
+    if((LPC_GPIO2->FIOPIN & (1<<12)) == 0){        //checking if sw2 is clicked
       LPC_GPIO0->FIOCLR=LED;      //clearing port
-      for(j=0;j<100000;j++);      //delay
+      for(j=0;j<10000;j++);      //delay
       LED -= 0x10;                //down counter
       if(LED==0x10)               //if goes at 0x10 start again from 0xFF0
           LED=0xFF0;  
     }
     else{
       LPC_GPIO0->FIOCLR=LED;      //clearing port
-      for(j=0;j<100000;j++);      //delay
+      for(j=0;j<10000;j++);      //delay
       LED += 0x10;                //up counter
       if(LED==(0x10<<8))          //if crosses 256 go back to 1
           LED=0x10;
     }
     LPC_GPIO0->FIOSET=LED;                          //Set LED value on
-    for(j=0;j<100000;j++);                          //delay
+    for(j=0;j<10000;j++);                          //delay
   }
 }
